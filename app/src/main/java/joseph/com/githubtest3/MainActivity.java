579b7b9af3533af350d2f7ed2a7515d228bee226
Brian <brncherin@gmail.com>
2016-11-19 12:36:42 -0500

    toast button makes toast

--- app/src/main/java/joseph/com/githubtest3/MainActivity.java
+++ app/src/main/java/joseph/com/githubtest3/MainActivity.java
@@ -1,6 +1,9 @@
 package joseph.com.githubtest3;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -15,10 +18,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private Button button;
     private int n;
 
+
+    Button toastButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toastButton = (Button)findViewById(R.id.toastButton);
+
+        toastButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "TOASTTOAWSTTOAST", Toast.LENGTH_SHORT).show();
+            }
+        });
+
 
         textView = (TextView) findViewById(R.id.number);
         button = (Button) findViewById(R.id.test);
